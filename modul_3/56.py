5ba8c9e764caf966e88662cc2aee6ec214e29cec
jaydipchavada <jaydipchavda113@gmail.com>
2023-07-17 16:00:05 +0530

    done

--- modul_3/56.py
+++ modul_3/56.py
@@ -5,6 +5,6 @@ import random
 # Set the seed value
 random.seed(13)
 
-# Generate random numbers same for seed value 12
+# Generate random numbers same for seed value 13
 random_number = random.randint(1,100)
 print(random_number)
